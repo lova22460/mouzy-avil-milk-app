193929044d492d3d437f328264adcf2b29027692
lova22460 <lovaaimusic22@gmail.com>
2026-09-18 11:07:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -654,9 +654,6 @@ def login():
 
     <head>
 
-    <link rel="manifest" href="/manifest.json">
-    <meta name="theme-color" content="#ffffff">
-
     <meta name="viewport"
           content="width=device-width, initial-scale=1">
 
